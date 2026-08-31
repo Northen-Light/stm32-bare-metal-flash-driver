aedc9474a11189808f0e66b881f7885ae52f9282
Prateek Patnaik <prateekpatnaikcomp@gmail.com>
2026-08-31 21:23:18 +0530

    Add data verification snippets for write,erase flash operations

--- src/main.c
+++ src/main.c
@@ -1,19 +1,34 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include "spi.h"
 #include "flash.h"
 
 #define DATA_BYTES_LENGTH                 4096
 
+typedef enum {
+  FLASH_VERIFICATION_STATUS_NOT_VERIFIED,
+  FLASH_VERIFICATION_STATUS_VERIFIED,
+  FLASH_VERIFICATION_STATUS_FAILURE
+} flash_verification_status_t;
+
 const uint32_t page_address = 0x0000E0;
 jedec_id_t id;
 uint8_t out_buffer[DATA_BYTES_LENGTH];
 uint8_t in_buffer[DATA_BYTES_LENGTH];
+flash_verification_status_t \
+  flash_write_verfication_status = FLASH_VERIFICATION_STATUS_NOT_VERIFIED;
+
+flash_verification_status_t \
+  flash_erase_verification_status = FLASH_VERIFICATION_STATUS_NOT_VERIFIED;
 
 void main(void) {
   flash_status_t status;
 
   spi1_init();
   flash_init();
+
+  flash_write_verfication_status = FLASH_VERIFICATION_STATUS_NOT_VERIFIED;
+  flash_erase_verification_status = FLASH_VERIFICATION_STATUS_NOT_VERIFIED;
 
   flash_read_jedec_id(&id);
 
@@ -23,6 +38,15 @@ void main(void) {
   
   if (status == FLASH_STATUS_OK) {
     flash_read(page_address, out_buffer, DATA_BYTES_LENGTH);
+
+    flash_erase_verification_status = FLASH_VERIFICATION_STATUS_VERIFIED;
+
+    for (uint32_t i = 0; i < DATA_BYTES_LENGTH; i++) {
+      if (out_buffer[i] != 0xFF) {
+        flash_erase_verification_status = FLASH_VERIFICATION_STATUS_FAILURE;
+        break;
+      } 
+    }
   }
 
   for (uint32_t index = 0; index < DATA_BYTES_LENGTH; index++) {
@@ -33,6 +57,15 @@ void main(void) {
 
   if (status == FLASH_STATUS_OK) {
     flash_read(page_address, out_buffer, DATA_BYTES_LENGTH);
+
+    flash_write_verfication_status = FLASH_VERIFICATION_STATUS_VERIFIED;
+
+    for (uint32_t i = 0; i < DATA_BYTES_LENGTH; i++) {
+      if (in_buffer[i] != out_buffer[i]) {
+        flash_write_verfication_status = FLASH_VERIFICATION_STATUS_FAILURE;
+        break;
+      }
+    }
   }
 
   while(1) {}
